a05f79c6cf96be640b48b699c3cd648310acc3be
Roman M. Koss <win.maklin@gmail.com>
2025-10-20 18:14:58 +0000

    feat(day1): update

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,135 @@
+// Note: instead of
+`
+node.next = this.head
+this.head.prev = node
+this.head = node
+`;
+// we use for simplicity, which does the same
+`[node.next, this.head.prev, this.head] = [this.head, node, node]`
+
+interface Node<T> {
+    value: T
+    prev?: Node<T>
+    next?: Node<T>
+}
+
 export default class DoublyLinkedList<T> {
-    public length: number;
-
+    public length: number = 0;
     
+    private head?: Node<T>
+    private tail?: Node<T>
 
-    constructor() {
-    }
+    constructor() {}
 
     prepend(item: T): void {
+        const node: Node<T> = { value: item }
 
-}
+        this.length++;
+
+        if (!this.head) {
+            this.head = this.tail = node;
+            return
+        }
+
+        [node.next, this.head.prev, this.head] = [this.head, node, node]
+    }
+
     insertAt(item: T, idx: number): void {
+        // guard clauses goes first for base cases
+        if (idx > this.length) {
+            throw new Error("Idx out of bounds")
+        }
+        if (idx === this.length) {
+            this.append(item)
+            return
+        }
+        if (idx === 0) {
+            this.prepend(item)
+            return;
+        }
 
-}
+        // we select next, until the final next which should be at idx position
+        const curr = this.getAt(idx)
+
+        // ex: a <=> b, we are inserting x on position b which has idx positioning
+        const node: Node<T> = { value: item };
+
+        // setup new node references
+        [node.next, node.prev] = [curr, curr?.prev]
+
+        // setup the preposition to the current to link to our node instead, ex: a.next = x
+        if (curr && curr.prev) curr.prev.next = node
+
+        // setup current to link back to our node, ex:
+        if (curr) curr.prev = node
+
+        this.length++
+    }
+
     append(item: T): void {
+        this.length++
+        const node: Node<T> = { value: item }
+        if (!this.head || !this.tail) {
+            this.head = this.tail = node
+            return
+        }
 
-}
+        [node.prev, this.tail.next, this.tail] = [this.tail, node, node]
+    }
+
     remove(item: T): T | undefined {
+        let curr = this.head
+        for (let i = 0; curr && i < this.length; i++) {
+            if (curr.value === item) break;
+            curr = curr.next
+        }
+        if (!curr) return
 
-}
+        this.removeNode(curr)
+
+        return curr?.value
+    }
+
     get(idx: number): T | undefined {
+        return this.getAt(idx)?.value
+    }
 
-}
     removeAt(idx: number): T | undefined {
+        if (idx >= this.length) throw new Error("idx is out of bounds")
 
-}
+        const curr = this.getAt(idx)
+
+        if (curr) this.removeNode(curr)
+
+        return curr?.value
+    }
+    
+    private getAt(idx: number): Node<T> | undefined {
+        if (idx >= this.length) throw new Error("idx is out of bounds!")
+        if (idx === 0) return this.head
+        if (idx === this.length - 1) return this.tail
+
+        let curr = this.head
+        for (let i = 0; curr && i < idx; i++) {
+            curr = curr.next
+        }
+
+        return curr
+    }
+
+    private removeNode(node: Node<T>): void {
+        this.length--
+        if (this.length === 0) this.head = this.tail = undefined
+
+        if (node.prev) node.prev.next = node.next
+        if (node.next) node.next.prev = node.prev
+
+        if (node === this.head) this.head = node.next
+        if (node === this.tail) this.tail = node.prev
+
+        delete node.next
+        delete node.prev
+
+        node.value
+    }
 }
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,54 @@
-export default function quick_sort(arr: number[]): void {
+// So in quick sort we weak sort by selected pivot element.
+// everything on the left is less and equalt to the pivot and the everything on the right is bigger then pivot
+// then we select the part of the left and right, and make the same. 
+// Select pivot, set all then less and equal to the left, and all thet is more to the right
+// we do so, until we end up having only one item of the left, the pivot and the right
 
+function qs(arr: number[], lo: number, hi: number): void {
+    // we might hit out the low to be slightly above the hi
+    if (lo >= hi) return
+
+    // partition the current range
+    const pivotIdx = partition(arr, lo, hi)
+
+    // partition the low range
+    qs(arr, lo, pivotIdx - 1)
+    // partition the high range
+    qs(arr, pivotIdx + 1, hi)
+}
+
+// return number - pivot index
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi]
+
+    // we start from low, cause the books... there is a reason for that
+    let idx = lo - 1
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++ // we started at 0 position!
+            
+            // // swap old way
+            // const tmp = arr[i]
+            // arr[i] = arr[idx]
+            // arr[idx] = tmp
+            
+            // OR... swap with new way
+            [arr[i], arr[idx]] = [arr[idx], arr[i]]
+        }
+    }
+
+    idx++
+    
+    // arr[hi] = arr[idx]
+    // arr[idx] = pivot // arr[hi]
+
+    // OR! use destructor!
+    [arr[hi], arr[idx]] = [arr[idx], pivot] // same as = [arr[idx], arr[hi]]
+
+    return idx
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1)
 }
